63b96a023b8760bcbf9523ecf204c34eec2e96e8
CoderFek <praveen.anand2212@gmail.com>
2025-03-24 15:59:18 +0530

    using-mixins

--- src/core/views.py
+++ src/core/views.py
@@ -1,84 +1,68 @@
-from django.shortcuts import render
-from django.shortcuts import get_object_or_404
-
-from rest_framework.decorators import api_view
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework import generics, mixins
 from rest_framework.permissions import IsAuthenticated
 
 from .models import Plant
 from .serializers import PlantSerializer
 
-# Create your views here.
 
-# @api_view(['GET'])
-# def plant_list_view(request):
-#     user = request.user
+class PlantList(mixins.ListModelMixin,
+                mixins.CreateModelMixin,
+                generics.GenericAPIView):
     
-#     if user.is_authenticated:
-#         instance = Plant.objects.all()
-#         data = {}
-        
-#         if instance:
-#             data = PlantSerializer(instance, many=True).data
-#         return Response(data)
-        
-#     return Response({"error": "Authentication Error"}, status=401)
+    serializer_class = PlantSerializer
+    permission_classes = [IsAuthenticated]
 
-
-class PlantList(APIView):
-    '''
+    """
     Return plant list
-    '''
-    permission_classes = [IsAuthenticated]
+    """
 
-    def get(self, request, format=None):
-        
-        plants = Plant.objects.filter(owner=self.request.user)
-        data = PlantSerializer(plants, many=True).data
-        return Response(data)
-        
+    def get_queryset(self):
+        return Plant.objects.filter(owner=self.request.user)
+    
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs) 
 
-    '''
+    """
     Create a new plant
-    '''
-    def post(self, request, format=None):
+    """
+
+    def perform_create(self, serializer):   # overriding perform_create
+        serializer.save(owner=self.request.user)
+
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
         
-        new_plant = PlantSerializer(data=request.data)
-        if new_plant.is_valid():
-            new_plant.save(owner=self.request.user)   # save to db
-            return Response({"success": "Plant added"}, status=201)
-        return Response(new_plant.errors, status=400)
 
 
-class PlantDetail(APIView):
+class PlantDetail(mixins.RetrieveModelMixin,
+                  mixins.UpdateModelMixin,
+                  mixins.DestroyModelMixin,
+                  generics.GenericAPIView):
+    
+    serializer_class = PlantSerializer
     permission_classes = [IsAuthenticated]
 
-    '''
+    """
     Get plant details
-    '''
-    def get(self, request, pk, format=None):
-        plant = get_object_or_404(Plant, pk=pk, owner=self.request.user)
-        data = PlantSerializer(plant).data
-        return Response(data, status=200)
+    """
+
+    def get_queryset(self):
+        return Plant.objects.filter(owner=self.request.user)
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
     
 
-    '''
+    """
     Update lant details
-    '''
-    def put(self, request, pk, format=None):
-        plant = get_object_or_404(Plant, pk=pk, owner=self.request.user)
-        data = PlantSerializer(plant, data=request.data, partial=True)
-        if data.is_valid():
-            data.save(owner=plant.owner)
-            return Response({"success": "Plant Updated"}, status=200)
-        return Response(data.errors, status=400)
+    """
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
     
 
-    '''
+    """
     Delete plant
-    '''
-    def delete(self, request, pk, format=None):
-        plant = get_object_or_404(Plant, pk=pk, owner=self.request.user)
-        plant.delete()
-        return Response({"success": "Plant deleted"}, status=204)
+    """
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
